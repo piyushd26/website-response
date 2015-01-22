6e8bc5942512ef748cd1c67a9832f9a905638f18
Phong Vu <manhnguyenduc@vccorp.vn>
2015-01-22 14:43:22 +0700

    new

---
+++ web-response.py
@@ -0,0 +1,26 @@
+from flask import Flask
+import requests
+import time
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+@app.route('/response/<website>')
+def w_response(website):
+
+    url = 'http://' + website
+
+    start = time.time()
+    r = requests.get(url)
+    r.raw.read()
+    end = time.time()
+    t = end - start
+    return '%d' % int(round(t * 1000))
+
+
+if __name__ == '__main__':
+    app.run()
